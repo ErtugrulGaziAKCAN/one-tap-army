3f9f40fb32d039423b20b98568dcefc032e5ac2a
ErtugrulGaziAKCAN <ertugrulgaziakcan@gmail.com>
2025-03-06 00:07:09 +0300

    RotateToRivalAction

---
+++ OneTapArmy/Assets/Scripts/AI_Controllers/AI_System/Actions/Attack/RotateToRivalAction.cs
@@ -0,0 +1,42 @@
+using AI_Controllers.DataHolder.Core;
+using QuickTools.Scripts.Extensions;
+using scriptable_states.Runtime;
+using UnityEngine;
+namespace AI_Controllers.AI_System.Actions.Attack
+{
+    [CreateAssetMenu(menuName = "Scriptable State Machine/Actions/Attack/RotateToRivalAction",
+        fileName = "new RotateToRivalAction")]
+    public class RotateToRivalAction : ScriptableAction
+    {
+//-------Public Variables-------//
+
+
+//------Serialized Fields-------//
+
+
+//------Private Variables-------//
+
+#region UNITY_METHODS
+
+#endregion
+
+
+#region PUBLIC_METHODS
+
+        public override void Act(StateComponent statesComponent)
+        {
+            statesComponent.TryGetComponent(out AIDataHolderCore dataHolder);
+            var aiTransform = dataHolder.AITransform;
+            aiTransform.rotation = Quaternion.Slerp(aiTransform.rotation, Quaternion.LookRotation(aiTransform.position
+                .WithY(0f)
+                .DirectionTo(dataHolder.ClosestRivalHealth.transform.position.WithY(0f))), Time.deltaTime * 30f);
+        }
+
+#endregion
+
+
+#region PRIVATE_METHODS
+
+#endregion
+    }
+}
